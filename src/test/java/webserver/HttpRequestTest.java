7d863f19876ca4be58c948e5ab958a3335d2ff86
Sangmin <sm9171@nate.com>
2022-12-15 22:59:49 +0900

    feat : HttpRequest 구현

---
+++ src/test/java/webserver/HttpRequestTest.java
@@ -0,0 +1,35 @@
+package webserver;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class HttpRequestTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws Exception {
+        FileInputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals(HttpMethod.GET, request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParameter("userId"));
+    }
+
+    @Test
+    public void request_POST() throws Exception {
+        FileInputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals(HttpMethod.POST, request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParameter("userId"));
+
+    }
+}
